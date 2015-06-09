92d5341b10141a167907241cea87a584f6f1c033
Aswini S <ashumeow@live.com>
2015-06-10 03:38:46 +0530

    add hello world

---
+++ basics/hello_world.hs
@@ -0,0 +1,19 @@
+main = putStrLn "Hello Meow!"
+
+{-
+
+## COMPILATION
+
+dell@DELL3521 /d/haskell/2014.2.0.0/bin
+$ ghc basics/hello_world.hs
+[1 of 1] Compiling Main             ( basics\hello_world.hs, basics\hello_world.
+o )
+Linking basics\hello_world.exe ...
+
+## EXECUTION
+
+dell@DELL3521 /d/haskell/2014.2.0.0/bin
+$ ./basics/hello_world
+Hello Meow!
+
+-}
